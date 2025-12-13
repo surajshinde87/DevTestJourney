977167a72288660c33dc52fa40241627032d15d1
Suraj Shinde <surajpshinde8767@gmail.com>
2025-12-13 14:08:28 +0530

    Day 8: CI/CD foundations – Maven build lifecycle and automated test execution

---
+++ day8/src/main/java/com/day8/Day8Application.java
@@ -0,0 +1,15 @@
+package com.day8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day8Application.class, args);
+	   System.out.println("Application Started Successfully");
+	   System.out.println("CI CD Foundations");
+	}
+
+}
