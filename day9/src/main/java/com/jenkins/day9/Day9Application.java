cc9d04f70de41f87e94a84de5f22c2661cfaaaae
Suraj Shinde <surajpshinde8767@gmail.com>
2025-12-15 11:01:59 +0530

    Day 9 - Jenkins Setup Successful

---
+++ day9/src/main/java/com/jenkins/day9/Day9Application.java
@@ -0,0 +1,14 @@
+package com.jenkins.day9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day9Application.class, args);
+		System.out.println("Jenkins Download Successfull using Docker");
+	}
+
+}
